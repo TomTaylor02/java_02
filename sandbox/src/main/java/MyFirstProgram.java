8fba097fed44c9559a263292ad0aca41468bb026
TomTaylor02 <39751668+TomTaylor02@users.noreply.github.com>
2018-06-14 20:11:02 -0700

    Добавленно несколько функций

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,28 @@
-public class MyFirstProgram{
+public class MyFirstProgram {
 
-	public static void main(String[] args) {	
-		System.out.println("Hello, world!");
-	}
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Yuriy");
 
+    double v = 5;
+    System.out.println("Пощадь квадрата со стороной " + v + " = " + area(v));
+
+
+    double a = 4;
+    double b = 6;
+		System.out.println("Пощадь прямоугольника со сторонами "+a +" и "+ b +" = " + area(a, b));
+  }
+
+	public static void hello (String somebody){
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+	public static double area(double len) {
+    return len * len;
+  }
+
+	public static double area(double a, double b) {
+    return a * b;
+  }
 }
